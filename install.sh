179242ea6a138671c271f09c93872d744867dd70
Nate Schainblatt <nate@bluearrowmail.com>
2024-05-26 03:47:47 -0400

    Add z direrctory jumper. Create installation script (starting point)

---
+++ install.sh
@@ -0,0 +1,14 @@
+# zsh theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# terminal fuzzy finder (activate with fzf command when installed)
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Helpful directory jump tool, after cd'ing around for a while after installing you should be able to do 'z directory' and it will know which one you want to jump to
+git clone git@github.com:rupa/z.git ~/z
+
+# Terminal multiplexer
+sudo apt install tmux
+
